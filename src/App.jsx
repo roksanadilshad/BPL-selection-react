79b2e20c539cce2f7b4f9993254f9fd4571cd6fb
Roksana Dilshad <roksanadilshad@gmail.com>
2025-09-22 22:08:53 +0600

    Add file & navbar

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css';
+import navImg from './assets/logo.png'
+import navCoin from './assets/dollar-1.png'
+
+function App() {
+ 
+  return (
+    <>
+      <div className="navbar w-11/12 mx-auto">
+  <div className="flex-1">
+    <img src={navImg} alt="" className='w-[60px] h-[60px]'/>
+  </div>
+  <div className="flex">
+    <ul className="flex justify-between items-center">
+      <li className='mr-10 text-[16px] text-[#131313]'>
+        <a href="">Home</a>
+      </li>
+      <li className='mr-10 text-[16px] text-[#131313]'>
+        <a href="">Fixture</a>
+      </li>
+      <li className='mr-10 text-[16px] text-[#131313]'>
+        <a href="">Teams</a>
+      </li>
+      <li className='mr-10 text-[16px] text-[#131313]'>
+        <a href="">Schedules</a>
+      </li>
+    </ul>
+    <button className='btn flex justify-center items-center gap-2'>
+      <span>0</span>
+      <span> Coin</span>
+      <img src={navCoin} alt="" />
+    </button>
+
+  </div>
+</div>
+    </>
+  )
+}
+
+export default App
